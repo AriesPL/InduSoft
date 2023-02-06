b4feb19d1c1e3fb81b3ff7ea1d44240621618276
DESKTOP-QIABJ3G\bengo <bengogo@bk.ru>
2023-02-06 11:08:14 +0700

    Добавьте файлы проекта.

---
+++ InduSoft/Employee.cs
@@ -0,0 +1,12 @@
+﻿namespace InduSoft
+{
+	internal class Employee
+	{
+		public int Id { get; set; }
+		public int DepartmentId { get; set; }
+		public int? ChiefId { get; set; }
+		public string Name { get; set; }
+		public decimal OldSalary { get; set; }
+		public decimal NewSalary { get; set; }
+	}
+}
